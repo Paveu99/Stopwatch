d63298216215260db4d1d69479d11958df3735ec
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-15 20:10:46 +0200

    StopwatchView.tsx uses Stopwatches.tsx component

--- src/views/StopwatchView.tsx
+++ src/views/StopwatchView.tsx
@@ -1,17 +1,7 @@
 import '../styles/StopwatchView.scss'
+import {Stopwatches} from "../components/stopwatches/Stopwatches.tsx";
 export const StopwatchView = () => {
     return <div className="main_view">
-        <header className="upper-stopwatch">
-            <section></section>
-            <section className="smaller-stopwatch">
-                00:00:00
-            </section>
-        </header>
-        <div className="bigger-stopwatch">
-            00:00:00
-        </div>
-        <footer className="buttons">
-            BUTTONS
-        </footer>
+        <Stopwatches/>
     </div>
 }
