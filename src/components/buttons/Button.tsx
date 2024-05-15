98c21b2468758837b99070c6aafc666752adb195
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-15 20:09:37 +0200

    Change in className for single-button from click-button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export const Button = ({onClick, text, color, disabled}: Props) => {
-    return <button className="click-button" onClick={onClick} disabled={disabled} style={{backgroundColor: `${color}`}}>
+    return <button className="single-button" onClick={onClick} disabled={disabled} style={{backgroundColor: `${color}`}}>
         {text}
     </button>
 }
