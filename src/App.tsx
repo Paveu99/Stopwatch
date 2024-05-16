c7f0795f6d7b49202cfd7777d9d3839ad80539af
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-16 21:22:58 +0200

    Gathering data from children components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.scss'
 import {StopwatchView} from "./views/StopwatchView.tsx";
+import {useState} from "react";
+import {LeaderBoard} from "./views/LeaderBoard.tsx";
 
 function App() {
 
+  const [laps, setLaps] = useState<string[]>([]);
+
+  function addLap(lapsFromChild: string[]) {
+    setLaps(lapsFromChild)
+  }
+
   return <div className="page">
-    <StopwatchView/>
-    <div>LOGS</div>
+    <StopwatchView addLap={addLap}/>
     <LeaderBoard laps={laps}/>
   </div>
 }
