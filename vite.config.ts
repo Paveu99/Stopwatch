a8836a6e19264721f647d76381b2f40816a31c0d
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-18 13:34:48 +0200

    Enabling using mobile devices

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  server: {
+    host: command === 'serve' ? '0.0.0.0' : 'localhost',
+  },
+}));
